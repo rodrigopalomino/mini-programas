050add9337f02530cd2df19125d89e279fedc624
rodrigopalomino <117995998+rodrigopalomino@users.noreply.github.com>
2023-07-14 18:21:16 -0500

    sidebar terminado pero falta limpieza de codigo

--- sidebar/index.js
+++ sidebar/index.js
@@ -3,27 +3,81 @@ const bar = document.getElementById('icono-bar')
 const spans = document.querySelectorAll('.sidebar__inferior-grupo-span')
 console.log("botones : ",spans)
 
-var c = 1
+var c = true
+const controladores = {
+    house: false,
+    about:false,
+    messages:false,
+    photos:false,
+    settings:false
+}
+
+
 bar.addEventListener('click', (e) => {
     
 
-    if ( c%2!==0 ){//c imapr
+    if ( c ){//c true
+
         spans.forEach(span => {
             span.style.opacity = 0
         })
         e.target.classList.remove('fa-x')
         e.target.classList.add('fa-bars')
         document.getElementById('sidebar').classList.add('sidebar-activo')
-        c++
+        c = false
 
-    }else {//c par
+        if ( controladores.house ){
+            document.getElementById(`sidebar__inferior-grupo-house`).classList.remove('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-house`).classList.add('sidebar__inferior-grupo-activo-c')
+        }
+        if ( controladores.about ){
+            document.getElementById(`sidebar__inferior-grupo-about`).classList.remove('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-about`).classList.add('sidebar__inferior-grupo-activo-c')
+        }
+        if ( controladores.messages ){
+            document.getElementById(`sidebar__inferior-grupo-messages`).classList.remove('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-messages`).classList.add('sidebar__inferior-grupo-activo-c')
+        }
+        if ( controladores.photos ){
+            document.getElementById(`sidebar__inferior-grupo-photos`).classList.remove('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-photos`).classList.add('sidebar__inferior-grupo-activo-c')
+        }
+        if ( controladores.settings ){
+            document.getElementById(`sidebar__inferior-grupo-settings`).classList.remove('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-settings`).classList.add('sidebar__inferior-grupo-activo-c')
+        }
+
+
+    }else {//c false
+
         spans.forEach(span => {
             span.style.opacity = 1
         })
         e.target.classList.add('fa-x')
         e.target.classList.remove('fa-bars')
         document.getElementById('sidebar').classList.remove('sidebar-activo')
-        c++
+        c = true
+
+        if ( controladores.house ){
+            document.getElementById(`sidebar__inferior-grupo-house`).classList.add('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-house`).classList.remove('sidebar__inferior-grupo-activo-c')
+        }
+        if ( controladores.about ){
+            document.getElementById(`sidebar__inferior-grupo-about`).classList.add('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-about`).classList.remove('sidebar__inferior-grupo-activo-c')
+        }
+        if ( controladores.messages ){
+            document.getElementById(`sidebar__inferior-grupo-messages`).classList.add('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-messages`).classList.remove('sidebar__inferior-grupo-activo-c')
+        }
+        if ( controladores.photos ){
+            document.getElementById(`sidebar__inferior-grupo-photos`).classList.add('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-photos`).classList.remove('sidebar__inferior-grupo-activo-c')
+        }
+        if ( controladores.settings ){
+            document.getElementById(`sidebar__inferior-grupo-settings`).classList.add('sidebar__inferior-grupo-activo')
+            document.getElementById(`sidebar__inferior-grupo-settings`).classList.remove('sidebar__inferior-grupo-activo-c')
+        }
     }
     
 
@@ -36,7 +90,6 @@ bar.addEventListener('click', (e) => {
 iconos.forEach( icono => {
     icono.addEventListener('click', (e)=>{
 
-        console.log("sd : ",e.target.id)
 
         document.getElementById(`sidebar__inferior-grupo-house`).classList.remove('sidebar__inferior-grupo-activo')
         document.getElementById(`sidebar__inferior-grupo-about`).classList.remove('sidebar__inferior-grupo-activo')
@@ -44,23 +97,102 @@ iconos.forEach( icono => {
         document.getElementById(`sidebar__inferior-grupo-photos`).classList.remove('sidebar__inferior-grupo-activo')
         document.getElementById(`sidebar__inferior-grupo-settings`).classList.remove('sidebar__inferior-grupo-activo')
 
-        switch(e.target.id){
-            case "icono-house":
-                document.getElementById(`sidebar__inferior-grupo-house`).classList.add('sidebar__inferior-grupo-activo')
-                break
-            case "icono-about":
-                document.getElementById(`sidebar__inferior-grupo-about`).classList.add('sidebar__inferior-grupo-activo')
-                break
-            case "icono-messages":
-                document.getElementById(`sidebar__inferior-grupo-messages`).classList.add('sidebar__inferior-grupo-activo')
-                break
-            case "icono-photos":
-                document.getElementById(`sidebar__inferior-grupo-photos`).classList.add('sidebar__inferior-grupo-activo')
-                break
-            case "icono-settings":
-                document.getElementById(`sidebar__inferior-grupo-settings`).classList.add('sidebar__inferior-grupo-activo')
-                break
+        document.getElementById(`sidebar__inferior-grupo-house`).classList.remove('sidebar__inferior-grupo-activo-c')
+        document.getElementById(`sidebar__inferior-grupo-about`).classList.remove('sidebar__inferior-grupo-activo-c')
+        document.getElementById(`sidebar__inferior-grupo-messages`).classList.remove('sidebar__inferior-grupo-activo-c')
+        document.getElementById(`sidebar__inferior-grupo-photos`).classList.remove('sidebar__inferior-grupo-activo-c')
+        document.getElementById(`sidebar__inferior-grupo-settings`).classList.remove('sidebar__inferior-grupo-activo-c')
+
+        if(c) {
+            switch(e.target.id){
+                case "icono-house":
+                    document.getElementById(`sidebar__inferior-grupo-house`).classList.add('sidebar__inferior-grupo-activo')
+                    controladores.house = true
+                    controladores.about = false
+                    controladores.messages = false
+                    controladores.photos = false
+                    controladores.settings = false
+                    break
+                case "icono-about":
+                    document.getElementById(`sidebar__inferior-grupo-about`).classList.add('sidebar__inferior-grupo-activo')
+                    controladores.house = false
+                    controladores.about = true
+                    controladores.messages = false
+                    controladores.photos = false
+                    controladores.settings = false
+                    break
+                case "icono-messages":
+                    document.getElementById(`sidebar__inferior-grupo-messages`).classList.add('sidebar__inferior-grupo-activo')
+                    controladores.house = false
+                    controladores.about = false
+                    controladores.messages = true
+                    controladores.photos = false
+                    controladores.settings = false
+                    break
+                case "icono-photos":
+                    document.getElementById(`sidebar__inferior-grupo-photos`).classList.add('sidebar__inferior-grupo-activo')
+                    controladores.house = false
+                    controladores.about = false
+                    controladores.messages = false
+                    controladores.photos = true
+                    controladores.settings = false
+                    break
+                case "icono-settings":
+                    document.getElementById(`sidebar__inferior-grupo-settings`).classList.add('sidebar__inferior-grupo-activo')
+                    controladores.house = false
+                    controladores.about = false
+                    controladores.messages = false
+                    controladores.photos = false
+                    controladores.settings = true
+                    break
+            }
         }
+
+        if (!c){
+            switch(e.target.id){
+                    case "icono-house":
+                        document.getElementById(`sidebar__inferior-grupo-house`).classList.add('sidebar__inferior-grupo-activo-c')
+                        controladores.house = true
+                        controladores.about = false
+                        controladores.messages = false
+                        controladores.photos = false
+                        controladores.settings = false
+                        break
+                    case "icono-about":
+                        document.getElementById(`sidebar__inferior-grupo-about`).classList.add('sidebar__inferior-grupo-activo-c')
+                        controladores.house = false
+                        controladores.about = true
+                        controladores.messages = false
+                        controladores.photos = false
+                        controladores.settingss = false
+                        break
+                    case "icono-messages":
+                        document.getElementById(`sidebar__inferior-grupo-messages`).classList.add('sidebar__inferior-grupo-activo-c')
+                        controladores.house = false
+                        controladores.about = false
+                        controladores.messages = true
+                        controladores.photos = false
+                        controladores.settings = false
+                        break
+                    case "icono-photos":
+                        document.getElementById(`sidebar__inferior-grupo-photos`).classList.add('sidebar__inferior-grupo-activo-c')
+                        controladores.house = false
+                        controladores.about = false
+                        controladores.messages = false
+                        controladores.photos = true
+                        controladores.settings = false
+                        break
+                    case "icono-settings":
+                        document.getElementById(`sidebar__inferior-grupo-settings`).classList.add('sidebar__inferior-grupo-activo-c')
+                        controladores.house = false
+                        controladores.about = false
+                        controladores.messages = false
+                        controladores.photos = false
+                        controladores.settings = true
+                        break
+                }
+        }
+    
 
     })
 })
