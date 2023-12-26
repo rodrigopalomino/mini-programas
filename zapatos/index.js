ec6333b2341a9f94d73156918667ba51c5096abc
rodrigo <rodrigopalominocastro0@gmail.com>
2023-12-26 13:49:20 -0500

    agregando mas animaciones

--- zapatos/index.js
+++ zapatos/index.js
@@ -1,14 +1,14 @@
-const imagenes = document.querySelectorAll('.principal__secundario')
-const zapatoPrincipal = document.querySelector('.zapato-principal')
+const imagenes = document.querySelectorAll(".principal__secundario");
+const zapatoPrincipal = document.querySelector(".zapato-principal");
 
-console.log("ki : ",imagenes)
+console.log("ki : ", imagenes);
 
-imagenes.forEach(imagen => {
-    imagen.addEventListener('mouseenter', e => {
-        console.log("bine : ",e.target.id)
+imagenes.forEach((imagen) => {
+  imagen.addEventListener("mouseenter", (e) => {
+    console.log("bine : ", e.target.id);
 
-        const imagenSrc = e.target.querySelector('img').src
-        zapatoPrincipal.src = imagenSrc;
-        console.log("aca : ",imagenSrc)
-    })
-})
+    const imagenSrc = e.target.querySelector("img").src;
+    zapatoPrincipal.src = imagenSrc;
+    console.log("aca : ", imagenSrc);
+  });
+});
